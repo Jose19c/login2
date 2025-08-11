b31b99e4c0e5b01c22754013cef4be91c106df3c
Jose19c <ramirezjosejo394@gmail.com>
2025-08-11 12:49:38 -0600

    confirmo

--- phpLogin/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php //crea la conexion con la base de datos
-include("configuracion.php");
-$conexion = new mysqli($server,$user,$pass,$bd);
-if (mysqli_connect_errno()){
-    echo "No conectado", mysqli_connect_error();
-    exit();
-}
-?>
--- phpLogin/configuracion.php
+++
@@ -1,6 +0,0 @@
-<?php //los parametros para entrar a la base de datos
-$server = "mysql-systemaccesscontrol.alwaysdata.net";
-$user = "425802";
-$pass = "2325247502";
-$bd = "systemaccesscontrol_logindb";
-?>
